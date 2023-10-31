e29831858dcb20cd89f236fc6a07b45bec80d183
bwesterg <benton.westergaard@gmail.com>
2023-10-30 18:07:41 -0600

    fixes responsivity login issues

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Blog from './pages/Blog';
 import NoPage from './pages/NoPage';
 import './App.css';
 
@@ -19,6 +20,8 @@ export default function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+
             <Route path="/*" element={<NoPage />} />
 
           </Routes>
